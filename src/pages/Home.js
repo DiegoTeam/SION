3e50eeae6874d37d1a17e0dcacb4bf76e47eaf9a
CristianFonsec03 <cristian.lfs@gmail.com>
2020-03-22 16:49:21 -0500

    Action button integrate.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,21 @@ import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {Text} from 'react-native-elements';
+import ActionButton from 'react-native-action-button';
 
 const Home = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: '#f2f2f2'}}>
       <Text h1>Hola mundo</Text>
+      <ActionButton
+        buttonColor="#3B666F"
+        onPress={() => {
+          console.log('hi');
+        }}
+      />
     </View>
   );
 };
