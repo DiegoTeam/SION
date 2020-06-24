b7cc3e241e987baf0118d4b2e07ab93230e596e5
Cristian Fonseca <cristian.lfs@gmail.com>
2020-06-24 00:21:33 -0500

    Se permite editar el valor de iraca a 0 en las lineas

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './src/pages/App';
 import {name as appName} from './app.json';
+import {YellowBox} from 'react-native';
+YellowBox.ignoreWarnings([
+  'Remote debugger is in a background tab which may cause apps to perform slowly',
+  'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
